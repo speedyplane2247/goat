4a3f7a048721ea901bb5214a6842ea02cb5e8580
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-02-16 22:51:54 -0500

    Create goat-m.asm

---
+++ dos/goat-m.asm
@@ -0,0 +1,11 @@
+; if you are using debug.exe to assemble these, use goat-d.asm! These contains comments that don't work in debug.exe
+int 21h
+db "592 goat file: MS-DOS", 1
+db "goat-m.asm", 2
+db "Fork: github.com/speedyplane2247/goat", 3
+db "Master: github.com/speedyplane2247/goat", 4 ; Don't remove!
+db "This file should not print out anything.", 5
+db "Thanks for using this DOS GOAT file!", 6
+db "END OF FILE", 10
+int 21h
+; super simple, does nothing but is 592 bytes!
