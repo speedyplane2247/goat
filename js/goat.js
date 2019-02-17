35bf152c121b0bbe665025a6bc3cb58c6972eb2f
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-02-17 01:08:13 -0500

    Create goat.js

---
+++ js/goat.js
@@ -0,0 +1,12 @@
+// Windows 9X JavaScript GOAT
+// (c) speedyplane2247 2017-2019
+// github.com/speedyplane2247/goat
+// github.com/speedyplane2247/goat (modifiers page)
+var GoatYes = 0
+var Version "1.0"
+function goatGo() {
+alert("GOAT FILE: 269 bytes.")
+GoatYes = 1
+}
+// JUNK CODE
+// HERE
