42f73b8cef290b7779b3d254e1b824a99d66b187
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-02-17 00:29:41 -0500

    Add files via upload

---
+++ vb2015/VisualBasicGoatGUI/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+    Dim actualData As String = Nothing
+    Dim spacerData As String = Nothing
+    Dim headerData As String = Nothing
+    Dim dummyData As String = Nothing
+
+    Public Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        actualData = "This is the fake data. Something went wrong. (Maybe you didn't extract the data?)"
+        dummyData = "This is the fake data. Something went very wrong."
+        headerData = "Self-coded Data:"
+        spacerData = " "
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        actualData = "This is the real data. The file size is: 16,384."
+    End Sub
+
+    Public Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Try
+            Label2.Text = headerData + spacerData + actualData
+        Catch
+            MsgBox("Something went wrong.")
+        End Try
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MsgBox("Test GOAT file: The file size should be: 16,384.")
+    End Sub
+End Class
