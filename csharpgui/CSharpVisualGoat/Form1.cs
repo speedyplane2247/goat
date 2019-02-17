2e8fcd771da247e6540303cf06aea99402b6bf9c
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-02-16 23:44:28 -0500

    Add files via upload

---
+++ csharpgui/CSharpVisualGoat/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpVisualGoat
+{
+    public partial class Form1 : Form
+    {
+        private int DataYes;
+        private string Data;
+        private MessageBox MessageBox1;
+        public Form1()
+        {
+            InitializeComponent();
+            DataYes = 0;
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+             DataYes = 1;
+            try
+            {
+                Data = "This is some cool test data. This file should be 17,408 long!";
+            } catch
+            {
+                MessageBox.Show("Failed to get data!");
+            }
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try {
+                label2.Text = Data;
+            }
+            catch
+            {
+                MessageBox.Show("Failed to show data! Maybe it doesn't exist?");
+            }
+        }
+    }
+}
