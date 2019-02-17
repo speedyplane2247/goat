63e94ef329d1da8c40f790c9e59b165cf3d694a3
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-02-16 22:52:21 -0500

    Create goat-d.asm

---
+++ dos/goat-d.asm
@@ -0,0 +1,9 @@
+int 21h
+db "592 goat file: MS-DOS", 1
+db "goat-m.asm", 2
+db "Fork: github.com/speedyplane2247/goat", 3
+db "Master: github.com/speedyplane2247/goat", 4 ; Don't remove!
+db "This file should not print out anything.", 5
+db "Thanks for using this DOS GOAT file!", 6
+db "END OF FILE", 10
+int 21h
